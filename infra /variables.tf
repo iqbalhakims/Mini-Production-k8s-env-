be5b0e25b39d53245fb2ee1b407f62d013e060b3
Iqbal Fauzi <teamstac350@gmail.com>
2026-02-27 17:38:00 +0800

    update: variable.tf

--- infra /variables.tf
+++ infra /variables.tf
@@ -1,4 +1,15 @@
-variable "aws_region"{
-    type = string 
-    default = "ap-southeast-1"
+variable "aws_region" {
+  default = "ap-southeast-1"
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+variable "key_name" {
+  description = "Existing AWS keypair"
+}
+
+variable "ami" {
+  default = "ami-0df7a207adb9748c7" # Ubuntu 22.04 (update if needed)
 }
