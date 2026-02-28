c8b750cb9b0e7b6fd2180a1c5adfad9d4ddcbc67
Iqbal Fauzi <teamstac350@gmail.com>
2026-02-28 23:01:51 +0800

    feat: add public key

--- infra/main.tf
+++ infra/main.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "controlplane" {
 }
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name_controlplane
-  public_key = file(pathexpand("~/.ssh/id_ed25519.pub"))
+  public_key = var.public_key
 }
 
 resource "aws_instance" "workers" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -10,6 +10,11 @@ variable "key_name_controlplane" {
   default = "id_ed25519.pub"
 }
 
+variable "public_key" {
+  description = "SSH public key content"
+  sensitive   = true
+}
+
 variable "ami" {
   default = "ami-0df7a207adb9748c7" # Ubuntu 22.04 (update if needed)
 }
